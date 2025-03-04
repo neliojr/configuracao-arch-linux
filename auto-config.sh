14709610d4df8e032ad18a4f62ab952a918b1485
Nelio Júnior <neliojr@neliojr.me>
2025-03-04 20:56:12 -0300

    atualizando script de configuração automática para incluir o wget e ativar o repositório multilib

--- auto-config.sh
+++ auto-config.sh
@@ -9,7 +9,13 @@ sleep 10
 sudo pacman -Syu --noconfirm
 
 # Instalando pacotes
-sudo pacman -S --noconfirm bluez bluez-utils bluez-tools blueman rclone dkms linux-headers neofetch git timeshift fuse2 vlc ncdu putty docker docker-compose croc gnome-browser-connector flatpak cronie gnome-boxes
+sudo pacman -S --noconfirm bluez bluez-utils bluez-tools blueman rclone dkms linux-headers neofetch wget git timeshift fuse2 vlc ncdu putty docker docker-compose croc gnome-browser-connector flatpak cronie gnome-boxes
+
+# Ativando o multilib
+sudo sed -i '/^\[multilib\]/,/Include/s/^#//' /etc/pacman.conf
+
+# Atualizando repositório
+sudo pacman -Syu --noconfirm
 
 # Instalando yay (AUR helper)
 cd /home/nelio/Downloads/
